704e72869ed944b71059bdd43b07ec86762edf6b
hoonih <hoonih1004@gmail.com>
2023-06-22 02:36:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sunrinton/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.sunrinton
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.example.sunrinton.databinding.ActivityMainBinding
+import com.example.sunrinton.databinding.ActivityTestBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.loginbutton.setOnClickListener {
+            Toast.makeText(this, "버튼을 눌렀습니다. ", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, test::class.java)
+            var id = binding.edittextid.text.toString()
+            var pw = binding.edittextpassword.text.toString()
+
+            intent.putExtra("id", "${id}")
+            intent.putExtra("pw", "${pw}")
+            startActivity(intent)
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sunrinton/test.kt
@@ -0,0 +1,25 @@
+package com.example.sunrinton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sunrinton.databinding.ActivityMainBinding
+import com.example.sunrinton.databinding.ActivityTestBinding
+
+class test : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityTestBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val id = intent.getStringExtra("id")
+        val pw = intent.getStringExtra("pw")
+        binding.edittext1.setText("아이디: ${id}\n패스워드: ${pw}")
+
+    }
+}
